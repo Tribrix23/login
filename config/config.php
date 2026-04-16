e57dbcf8d10e49056e13f94b6995d9d5bde9879f
John David L. Perez <tribrix23@gmail.com>
2026-04-16 22:49:09 +0800

    applying dbb and config

--- config/config.php
+++ config/config.php
@@ -1,12 +1,18 @@
 <?php
 
-define ("DB_HOST", "localhost");
-define ("DB_NAME", "users");
-define ("DB_USER", "root");
-define ("DB_PASS", "");
+define("DB_HOST", "localhost");
+define("DB_NAME", "users");
+define("DB_USER", "root");
+define("DB_PASS", "");
 
 
-define ("BASE_URL", "localhost/login");
+define("BASE_URL", "localhost/login");
 
+define("SITE_NAME", "Project Web");
+
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 ?>
--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
+include "config/config.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="styles/global.css">
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
